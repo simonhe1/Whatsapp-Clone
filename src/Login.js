52919442b2a19bafef9a1b6a1d64d26fc79322ca
Simon <simonhe1@mail.com>
2020-09-20 22:26:54 -0400

    Added email and password auth

--- src/Login.js
+++ src/Login.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "@material-ui/core";
 import "./Login.css";
 import { auth, provider } from "./firebase";
 import { useStateValue } from "./StateProvider";
 import { actionTypes } from "./reducer";
+import { useHistory } from "react-router-dom";
 
 const Login = () => {
   const [{}, dispatch] = useStateValue();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const history = useHistory();
 
-  const signIn = () => {
+  const signInWithEmailAndPassword = (event) => {
+    event.preventDefault();
+
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then((auth) => {
+        if (auth) {
+          history.push("/");
+        }
+      })
+      // Will update with display message in the future
+      .catch((err) => console.log(err));
+  };
+
+  const signInWithGoogle = () => {
     auth
       .signInWithPopup(provider)
       .then((result) => {
@@ -18,6 +36,20 @@ const Login = () => {
         });
       })
       .catch((err) => console.log(err.message));
+  };
+
+  const register = (event) => {
+    event.preventDefault();
+
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((auth) => {
+        // It successfully created a new user with email and password
+        if (auth) {
+          history.push("/");
+        }
+      })
+      .catch((err) => console.log(err));
   };
 
   return (
@@ -30,8 +62,43 @@ const Login = () => {
         <div className="login_text">
           <h1>Sign in to WhatsApp</h1>
         </div>
+        <form>
+          <h5>Email</h5>
+          <input
+            type="text"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
 
-        <Button onClick={signIn}>Sign In With Google</Button>
+          <h5>Password</h5>
+          <input
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
+
+          <button
+            type="submit"
+            className="login_sign_in_button"
+            onClick={signInWithEmailAndPassword}
+          >
+            Sign In
+          </button>
+        </form>
+
+        <button
+          className="login_register_email_and_pass_button"
+          onClick={register}
+        >
+          Create your Whatsapp Account
+        </button>
+
+        <Button
+          onClick={signInWithGoogle}
+          className="login_register_google_button"
+        >
+          Sign In With Google
+        </Button>
       </div>
     </div>
   );
